ea4971f81a7b2fef265dc419cb68bf457ca979b1
AA\tzurlind <7946625+tjzurlin@users.noreply.github.com>
2024-02-23 16:13:13 -0500

    Fix compilation error for mod on windows

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,10 @@
     message("The mod had been created on Linux.")
   } else {
     if (.Platform$OS.type == 'windows') {
-      system(paste("gcc -o", shQuote(paste(file.path(system.file(package = "RMCSim"), "bin"), "mod.exe", sep="/")), shQuote(paste(file.path(system.file(package = "RMCSim"), "mod"), "*.c", sep="/")), sep=" "))
+      c.files <- c("getopt.c", "lex.c", "lexerr.c", "lexfn.c", "mod.c", "modd.c", "modi.c", "modiSBML.c", "modiSBML2.c", "modo.c", "strutil.c")
+      path.to.files = paste(file.path(system.file(package = "RMCSim"), "mod"), '/', sep="")
+      #system(paste("gcc -o", shQuote(paste(file.path(system.file(package = "RMCSim"), "bin"), "mod.exe", sep="/")), shQuote(paste(file.path(system.file(package = "RMCSim"), "mod"), c.files, sep="/")), sep=" "))
+      system(paste("gcc", paste0('"', path.to.files, c.files, '"', collapse = " "), '-o', shQuote(paste(file.path(system.file(package = "RMCSim"), "bin"), "mod.exe", sep="/")), sep=" "))
     }
     else if (.Platform$OS.type == 'unix') {
       system(paste("gcc -o", paste(file.path(system.file(package = "RMCSim"), "bin"), "mod", sep="/"), paste(file.path(system.file(package = "RMCSim"), "mod"), "*.c", sep="/"), sep=" "))
