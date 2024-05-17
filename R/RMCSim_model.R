fd81efdf3cd5245b95b62ad8c1cc21a8f299abab
AA\tzurlind <7946625+tjzurlin@users.noreply.github.com>
2024-05-17 08:45:45 -0400

    Update RMCSim to use setRefClass object for each model type

--- R/RMCSim_model.R
+++ R/RMCSim_model.R
@@ -2,17 +2,66 @@
 #' 
 #' A class for managing RMCSim models
 #' 
-#' @export
-RMCSimModel <- function(model_name) {
-    obj <- list()
-    class(obj) <- "RMCSimModel"
-    obj$mName <- model_name
-    obj$dll_name <- NULL
-    obj$dll_file <- NULL
-    obj$inits_file <- NULL
-
-    obj$initParms <- NULL
-    obj$initStates <- NULL
-    obj$Outputs <- NULL
-    return(obj)
-}
+#' @import methods
+#' @import deSolve
+#' @export RMCSimModel
+  RMCSimModel <- setRefClass("RMCSimModel", 
+                             fields=list(mName='character', initParms='function', initStates='function', Outputs='character'),
+                             methods = list(
+                               
+                               load_model=function() {
+                                 # Construct names of required files and objects from mName.
+                                 dll_name <- paste(mName, "_model", sep="")
+                                 dll_file <- paste(dll_name, .Platform$dynlib.ext, sep="")
+                                 inits_file <- paste(dll_name, "_inits.R", sep="")
+                                 
+                                 # Construct the compiled model names
+                                 model_file <- paste(mName, ".model", sep="")
+                                 c_file <- paste(mName, "_model.c", sep="")
+                                 dll_name <- paste(mName, "_model", sep="")
+                                 dll_file <- paste(dll_name, .Platform$dynlib.ext, sep="")
+                                 
+                                 if (!file.exists(dll_file)) {
+                                   compile_model(model_file, c_file, dll_name, dll_file)
+                                 }
+                                 
+                                 # Logic for compiling here if trying to load an uncompiled model
+                                 
+                                 # Load the compiled model (DLL).
+                                 dyn.load(dll_file)
+                                 
+                                 # Run script that defines initialization functions.
+                                 source(inits_file, local=TRUE)
+                                 initParms <<- initParms
+                                 initStates <<- initStates
+                                 Outputs <<- Outputs
+                               
+                             }, 
+                             
+                             run_model=function(times, Y0=NULL, parms=NULL, rtol=1e-6, atol=1e-6, maxsteps=5000,
+                                                forcing=NULL, fcontrol=NULL, event_list=NULL, method="lsoda") {
+                                 # Construct DLL name from mName.
+                                 dll_name <- paste(mName, "_model", sep="")
+                                 
+                                 # If parameter values are not provided, use default values.
+                                 if (is.null(parms)) {
+                                   parms <- initParms()
+                                 }
+                                 
+                                 # If initial values for state variables are not provided, use default
+                                 # values.
+                                 if (is.null(Y0)) {
+                                   Y0 <- initStates(parms)
+                                 }
+                                 
+                                 # Solve the ODE system using the "ode" function from the package "deSolve".
+                                 out <- ode(Y0, times, func="derivs", parms=parms, rtol=rtol, atol=atol, maxsteps=maxsteps,
+                                            dllname=dll_name, initforc="initforc", forcing=forcing,
+                                            fcontrol=fcontrol, initfunc="initmod", nout=length(Outputs),
+                                            outnames=Outputs, events=event_list, method=method)
+                                 
+                                 # Return the simulation output.
+                                 return(out)
+                             }
+                               )
+                             )
--- R/load_model.R
+++ R/load_model.R
@@ -10,39 +10,31 @@
 #'   file should be provided.
 #'
 #' @export
-
-load_model <- function(model) {
-  mName = model$mName
+load_model <- function() {
   # Construct names of required files and objects from mName.
-  model$dll_name <- paste(mName, "_model", sep="")
-  model$dll_file <- paste(model$dll_name, .Platform$dynlib.ext, sep="")
-  model$inits_file <- paste(model$dll_name, "_inits.R", sep="")
+  dll_name <- paste(mName, "_model", sep="")
+  dll_file <- paste(dll_name, .Platform$dynlib.ext, sep="")
+  inits_file <- paste(dll_name, "_inits.R", sep="")
 
   # Construct the compiled model names
-  model$model_file = paste(mName, ".model", sep="")
-  model$c_file = paste(mName, "_model.c", sep="")
-  model$dll_name = paste(mName, "_model", sep="")
-  model$dll_file = paste(model$dll_name, .Platform$dynlib.ext, sep="")
+  model_file <- paste(mName, ".model", sep="")
+  c_file <- paste(mName, "_model.c", sep="")
+  dll_name <- paste(mName, "_model", sep="")
+  dll_file <- paste(dll_name, .Platform$dynlib.ext, sep="")
 
-  if (!file.exists(model$dll_file)) {
-    compile_model(model)
+  if (!file.exists(dll_file)) {
+    compile_model(model_file, c_file, dll_name, dll_file)
   }
 
   # Logic for compiling here if trying to load an uncompiled model
 
   # Load the compiled model (DLL).
-  dyn.load(model$dll_file)
+  dyn.load(dll_file)
 
   # Run script that defines initialization functions.
-  source(model$inits_file)
-  model$initParms <- initParms
-  model$initStates <- initStates
-  model$Outputs <- Outputs
-  model
-
-  # Assign initialization functions and list of output variable names to the
-  # "global" environment.
-  #assign("initParms", initParms, envir=.GlobalEnv)
-  #assign("initStates", initStates, envir=.GlobalEnv)
-  #assign("Outputs", Outputs, envir=.GlobalEnv)
+  source(inits_file, local=TRUE)
+  initParms <<- initParms
+  initStates <<- initStates
+  Outputs <<- Outputs
+  
 }
