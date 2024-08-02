304e499f20eba32d95fd0f11eca0faaa1d088544
AA\tzurlind <7946625+tjzurlin@users.noreply.github.com>
2024-08-02 16:06:11 -0400

    Update Model object to accept paths to model file

--- R/MCSim_model.R
+++ R/MCSim_model.R
@@ -17,26 +17,22 @@ Model <- setRefClass("Model",
         stop("Cannot both have a model file `mName` and a model string `mString`")
       }
       if (length(mString) > 0) {
-        file <- tempfile(pattern = "mcsimmod_", tmpdir = '.')
-        mName <<- basename(file)
+        file <- tempfile(pattern = "mcsimmod_")#, tmpdir = '.')
         writeLines(mString, paste0(file, ".model"))
+      } else {
+        file <- normalizePath(mName)
       }
       
-      components <- strsplit(mName, "/")[[1]]
-      mName <<- components[length(components)] # mName is always the last entry
-      mPath <- if (length(components) > 1) {
-        normalizePath(paste0(paste(components[-length(components)], collapse = '/'), '/'))
-      } else {
-        normalizePath("./")
-      }
+      mName <<- basename(file)
+      mPath <- dirname(file)
       
       paths <<- list(
         dll_name = paste0(mName, "_model"),
-        c_file = paste0(mPath, mName, "_model.c"),
-        o_file = paste0(mPath, mName, "_model.o"),
-        dll_file = paste0(mPath, mName, "_model", .Platform$dynlib.ext),
-        inits_file = paste0(mPath, mName, "_model_inits.R"),
-        model_file = paste0(mPath, mName, ".model")
+        c_file = file.path(mPath, paste0(mName, "_model.c")),
+        o_file = file.path(mPath, paste0(mName, "_model.o")),
+        dll_file = file.path(mPath, paste0(mName, "_model", .Platform$dynlib.ext)),
+        inits_file = file.path(mPath, paste0(mName, "_model_inits.R")),
+        model_file = file.path(mPath, paste0(mName, ".model"))
       )
     },
     loadModel = function() {
