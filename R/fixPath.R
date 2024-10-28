ff02fc3b6d4bff1cda797984d6c68d579d19aa14
Andy Shapiro <shapiro.andy@epa.gov>
2024-10-28 16:12:53 -0400

    Revert "add naughty styling"
    This reverts commit 26daef8cf79f1bc19e68983608d667c40d09f627.

--- R/fixPath.R
+++ R/fixPath.R
@@ -13,15 +13,7 @@
 
   has_space <- grepl(" ", new.mPath)
   if (has_space == T) {
-
-
-
-
-    stop(
-      "Error: User-defined directory has space which will throw error for .dll/.so compilation"
-
-
-    )
+    stop("Error: User-defined directory has space which will throw error for .dll/.so compilation")
   }
 
   return(list("mPath" = new.mPath, "mName" = new.mName))
